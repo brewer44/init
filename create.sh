4f5a3e0eacc2c9e2a21a30e16f27175df60bf0c8
Brewer Johnson <brewerjohnson@gmail.com>
2022-09-09 15:00:01 -0600

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Auhtor: Brewer Johnson
+# Description: Local env creation
+# Version:
+#   1.0:May 12 2021: Inital script creation. 
+
+brew install --cask iterm2
+brew install --cask clipy
+brew install --cask spectacle
+investigate_pkgs="ack" 
+libs_pkgs="gettext gdbm icu4c id3lib libidn2 libunistring ncurses pcre2 readline wimlib"
+dev_pkgs="firefox git gitnote go jq node oniguruma openjdk openssl@1.1 python@3.9 sqlite vagrant vivaldi vagrant-completion visual-studio-code"
+kube_pkgs="helm kubernetes-helm rke rancher-cli"
+docker_pkgs="docker docker-completion"
+tools_pkgs="agedu csvkit fping lynx mysql-client nmap spectacle telnet vault watch wget"
+brew_per_pkgs="bash_completion discord keybase lens mpdecimal signal spotify tcl-tk tree wtf wtfutil xz youtube-dl youtube-to-mp3"
+
+echo "Installing Dev packages"
+for itr in $(echo $dev_pkgs); do
+    brew install $itr
+done
+echo "Installing Kube packages"
+for itr in $(echo $kube_pkgs); do
+    brew install $itr
+done
+echo "Installing Docker packages"
+for itr in $(echo $docker_pkgs); do
+    brew install $itr
+done
+echo "Install Tools"
+for itr in $(echo $tools_pkgs); do
+    brew install $itr
+done
+echo "Install personal packages"
+for itr in $(echo $brew_per_pkgs); do
+    brew install $itr
+done
+#echo "Just in case, installing other libs that were on legacy instances"
+#brew install $libs_pkgs
+ 
+#echo "*************************"
+#echo "Brew set up complete. Moving on to manual set ups"
+#echo ""
+
+echo "Grabbing bash_profile"
+#TODO: 
